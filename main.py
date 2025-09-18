562ec3d9a9008bddcd80950929fe64e7f4b3d4fd
Igor Gerasimov <gerasimov.igor.98@icloud.com>
2025-09-18 16:14:55 +0300

    test

--- main.py
+++ main.py
@@ -132,9 +132,10 @@ if __name__ == "__main__":
         from dotenv import load_dotenv
         load_dotenv()
     
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=settings.debug
-    )
+    # uvicorn.run(
+    #     "main:app",
+    #     host="0.0.0.0",
+    #     port=8000,
+    #     reload=settings.debug
+    # )
+    print('sucsess')
